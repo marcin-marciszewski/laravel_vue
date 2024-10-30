735e5eb42f46e66b01657b55d446e242cc13b6c6
Marcin Marciszewski <marciszewski.m@gmail.com>
2024-10-30 08:26:17 +0100

    first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Index</h1>
+  <h2>
+    {{ message }}
+  </h2>
+  <Link href="/hello">
+    Show Page
+  </Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+defineProps({
+    message: {
+        type: String,
+        default: 'No message',
+    },
+
+})
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Show</h1>
+  <Link href="/">Index Page</Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
